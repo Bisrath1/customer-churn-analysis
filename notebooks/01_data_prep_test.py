1816d4a1c8df12effe15718efdc7951b2e14013e
Bisrat Haile <bisrathaile1919@gmail.com>
2025-07-09 14:46:39 +0300

    ✅ Add test script for data loading and cleaning (Task 1)

---
+++ notebooks/01_data_prep_test.py
@@ -0,0 +1,19 @@
+# --- notebooks/01_data_prep_test.py ---
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from data_prep import load_data, clean_data
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'Telco_Customer_Churn_Dataset.csv'))
+df = load_data(file_path)
+print("Raw Data Sample:\n", df.head())
+
+df_clean = clean_data(df)
+print("\nCleaned Data Sample:\n", df_clean.head())
+print("\nCleaned Data Info:\n")
+print(df_clean.info())
+
+
+
+
+
